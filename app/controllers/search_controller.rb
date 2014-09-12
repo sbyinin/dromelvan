f1fdbdd6adbf2d9a94daf9838ef73b992be2fe08
dromelvan <dromelvan@aland.net>
2014-09-13 00:07:23 +0300

    D11Teams show up in search results.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,26 +3,26 @@ class SearchController < ApplicationController
   def search    
     @q = search_params[:q]
     
-    @players, @additional_player_count, @teams = execute_search(@q)
+    @players, @additional_player_count, @teams, @d11_teams = execute_search(@q)
   end
 
   def live_search    
     q = search_params[:q]
 
-    players, additional_player_count, teams = execute_search(q)
+    players, additional_player_count, teams, d11_teams = execute_search(q)
 
     if additional_player_count > 0
       # Add a dummy player for the additional player count.
       players.push(Player.new(last_name: "... and #{additional_player_count} additional players"))
     end
 
-    result = players + teams #+ stadia + users + seasons
+    result = players + teams + d11_teams #+ stadia + users + seasons
     
     result_json = Jbuilder.encode do | json |    
       json.array! result do | object |
         json.name object.name
         json.path polymorphic_path(object)
-        json.category object.class.name
+        json.category object.class.name.titleize.pluralize
         # Not using the icon in the view at the moment but sending it back anyway for now.
         json.icon icon_url(object)
       end      
@@ -47,8 +47,9 @@ private
       players = players[0..19]      
     end
     
-    teams = Team.named(q)    
-    return players, additional_player_count, teams
+    teams = Team.named(q)
+    d11_teams = D11Team.named(q)    
+    return players, additional_player_count, teams, d11_teams
   end
   
   def icon_url(object)
