8ae9a3a5cbbd431486b0f5a16ba1a1192ffe46b0
dromelvan <dromelvan@aland.net>
2014-09-17 08:30:44 +0300

    Match stuff.

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -6,8 +6,9 @@ describe MatchDay, type: :model do
   let(:away_team) { FactoryGirl.create(:team) }
   let(:match_day) { FactoryGirl.create(:match_day) }
   let(:stadium) { FactoryGirl.create(:stadium) }
+  let(:datetime) { DateTime.now }
   
-  before { @match = FactoryGirl.create(:match, home_team: home_team, away_team: away_team, match_day: match_day, stadium: stadium, home_team_goals: 1, away_team_goals: 2, datetime: DateTime.now, elapsed: "FT", status: 2, whoscored_id: 1) }
+  before { @match = FactoryGirl.create(:match, home_team: home_team, away_team: away_team, match_day: match_day, stadium: stadium, home_team_goals: 1, away_team_goals: 2, datetime: datetime, elapsed: "FT", status: 2, whoscored_id: 1) }
   
   subject { @match }
   
@@ -24,4 +25,54 @@ describe MatchDay, type: :model do
   
   it { is_expected.to be_valid }
     
+  describe '#home_team' do
+    subject { @match.home_team }
+    it { is_expected.to eq home_team }
+  end
+    
+  describe '#away_team' do
+    subject { @match.away_team }
+    it { is_expected.to eq away_team }
+  end
+    
+  describe '#match_day' do
+    subject { @match.match_day }
+    it { is_expected.to eq match_day }
+  end
+    
+  describe '#stadium' do
+    subject { @match.stadium }
+    it { is_expected.to eq stadium }
+  end
+
+  describe '#home_team_goals' do
+    subject { @match.home_team_goals }
+    it { is_expected.to eq 1 }
+  end
+
+  describe '#away_team_goals' do
+    subject { @match.away_team_goals }
+    it { is_expected.to eq 2 }
+  end
+
+  describe '#datetime' do
+    subject { @match.datetime }
+    it { is_expected.to eq datetime }
+  end
+
+  describe '#elapsed' do
+    subject { @match.elapsed }
+    it { is_expected.to eq "FT" }
+  end
+
+  describe '#status' do
+    subject { @match.status }
+    it { is_expected.to eq 2 }
+  end
+
+  describe '#whoscored_id' do
+    subject { @match.whoscored_id }
+    it { is_expected.to eq 1 }
+  end
+
 end
