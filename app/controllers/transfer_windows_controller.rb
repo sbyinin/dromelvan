bcbf50382e90d052cc2947e377df8df8b1b5673a
dromelvan <dromelvan@aland.net>
2015-01-12 13:55:39 +0200

    Added TransferWindow controller and views.

---
+++ app/controllers/transfer_windows_controller.rb
@@ -0,0 +1,3 @@
+class TransferWindowsController < ApplicationController
+  include Select
+end
---
+++ spec/views/transfer_windows_views_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "TransferWindow", type: :view do
+  
+  subject { page }
+
+  it_should_behave_like "show view", TransferWindow do
+    let(:h1_text) { resource.name }
+  end
+
+  pending "final show layout."
+  
+end
