b72ccc6e9dbd809547c1fa48456b9339d418fa3e
dromelvan <dromelvan@aland.net>
2015-04-13 00:58:20 +0300

    TODO

---
+++ spec/views/todo_views_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+describe "TODO", type: :view do
+  
+  pending "home"
+  
+  pending "home > posts"
+  
+  pending "home > rules"
+  
+  pending "home > about"
+  
+  pending "home > seasons"
+  
+  pending "home > seasons > <season>"
+  
+  pending "home > season > premier_league"
+  
+  pending "home > season > premier_league > <match_day>"
+  
+  pending "home > season > premier_league > match_day > <match>"
+  
+  pending "home > season > premier_league > team_table_stats > <season>"
+  
+  pending "home > season > d11_league"
+  
+  pending "home > season > d11_league > <d11_match_day>"
+  
+  pending "home > season > d11_league > d11_match_day > <d11_match>"
+  
+  pending "home > season > d11_league > d11_team_table_stats > <season>"
+
+  pending "home > season > d11_league > transfer_windows"
+  
+  pending "home > season > d11_league > transfer_windows > <transfer_window>"
+  
+  pending "home > season > d11_league > transfer_window > transfers > <transfer_day>"
+  
+  pending "home > season > d11_league > transfer_window > transfer_listings > <transfer_day>"
+  
+  pending "home > season > d11_league > transfer_window > transfer_bids > <transfer_day>"
+   
+  pending "home > teams"
+  
+  pending "home > teams > team > (team_season_squad_stats) > <season>"
+  
+  pending "home > teams > team > matches > <season>"
+  
+  pending "home > d11_teams"
+  
+  pending "home > d11_teams > <season>"
+  
+  pending "home > d11_teams > d11_team > (d11_team_season_squad_stats) > <season>"
+  
+  pending "home > d11_teams > d11_team > d11_matches > <season>"
+  
+  pending "home > players"
+
+  pending "home > players > player > (matches) > <season>"
+  
+  pending "home > players > player > transfers"
+
+  pending "home > players > player_season_stats > <season>"
+    
+end
