180b6265720472ec33b0c512a8a8fdd836451b4c
dromelvan <dromelvan@aland.net>
2015-05-26 16:44:06 +0300

    Todo.

--- spec/views/todo_views_spec.rb
+++ spec/views/todo_views_spec.rb
@@ -14,52 +14,76 @@ describe "TODO", type: :view do
   # /about
   
   pending "home > seasons"
-  
+  # /seasons
+
   pending "home > seasons > <season>"
-  
+  # /seasons/<id>
+
   pending "home > season > premier_league"
-  
-  pending "home > season > premier_league > <match_day>"
-  
-  pending "home > season > premier_league > match_day > <match>"
-  
+  # /premier-league/<id>
+
   pending "home > season > premier_league > team_table_stats > <season>"
-  
+  # /premier-league/<id>/table
+
+  pending "home > season > premier_league > <match_day>"
+  # /match-day/<id>
+
+  pending "home > season > premier_league > match_day > <match>"
+  # /match/<id>
+
   pending "home > season > d11_league"
-  
-  pending "home > season > d11_league > <d11_match_day>"
-  
-  pending "home > season > d11_league > d11_match_day > <d11_match>"
-  
+  # /d11-league/<id>
+
   pending "home > season > d11_league > d11_team_table_stats > <season>"
+  # /d11-league/<id>/table
 
   pending "home > season > d11_league > transfer_windows"
-  
+  # /d11-league/<id>/transfer-windows
+
+  pending "home > season > d11_league > <d11_match_day>"
+  # /d11-match-day/<id>
+
+  pending "home > season > d11_league > d11_match_day > <d11_match>"
+  # /d11-match/<id>
+
   pending "home > season > d11_league > transfer_windows > <transfer_window>"
-  
+  # /transfer-windows/<id>
+
   pending "home > season > d11_league > transfer_window > transfers > <transfer_day>"
+  # /transfer-day/<id>
   
   pending "home > season > d11_league > transfer_window > transfer_listings > <transfer_day>"
+  # /transfer-day/<id>/transfer-listings
   
   pending "home > season > d11_league > transfer_window > transfer_bids > <transfer_day>"
-   
+  # /transfer-day/<id>/bids
+
   pending "home > teams"
+  # /teams
   
   pending "home > teams > team > (team_season_squad_stats) > <season>"
+  # /teams/<id>/squad/<id>
   
   pending "home > teams > team > matches > <season>"
+  # /teams/<id>/matches/<id>
   
   pending "home > d11_teams"
+  # /d11-teams
   
   pending "home > d11_teams > <season>"
-  
+  # /season/<id>/d11-teams
+
   pending "home > d11_teams > d11_team > (d11_team_season_squad_stats) > <season>"
-  
+  # /d11-teams/<id>/squad/<id>
+
   pending "home > d11_teams > d11_team > d11_matches > <season>"
+  # /d11-teams/<id>/matches/<id>
   
   pending "home > players"
+  # /players
 
-  pending "home > players > player > (matches) > <season>"  
+  pending "home > players > player > (matches) > <season>"
+  # /player/<id>
   
   pending "home > players > player > transfers"
 
@@ -68,5 +92,6 @@ describe "TODO", type: :view do
   pending "player tooltip"
   
   pending "home > search results"
+  # /search
   
 end
