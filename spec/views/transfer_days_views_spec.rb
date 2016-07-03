0ca3df8506a90c507e196c7dbe36608df2aaa9bd
dromelvan <dromelvan@aland.net>
2016-07-03 15:04:27 +0300

    TransferDay bids view.

--- spec/views/transfer_days_views_spec.rb
+++ spec/views/transfer_days_views_spec.rb
@@ -69,5 +69,41 @@ describe "TransferDay", type: :view do
       it { is_expected.to have_selector("table.transfer-listings") }
     end          
   end
+
+
+
+  describe "transfer_bids view" do
+    let!(:transfer_day) { FactoryGirl.create(:transfer_day) }
+    
+    before { visit show_transfer_bids_transfer_day_path(transfer_day) }
+    
+    it { is_expected.to have_selector("div#transfer-day-transfer-bids") }
+    it { is_expected.to have_selector("div#transfer-day-most-expensive-transfers") }  
+    it { is_expected.to have_selector("div#transfer-day-top-transfer-listings") }
+    
+    context "when dependents don't exist" do
+      before { visit show_transfer_bids_transfer_day_path(transfer_day) }
+      
+      it { is_expected.not_to have_selector("table.transfers") }
+      it { is_expected.not_to have_selector("table.transfer-bids") }
+      it { is_expected.not_to have_selector("table.transfer-listings") }
+    end
+
+    context "when dependents exist" do
+      let!(:player) { FactoryGirl.create(:player) }
+      let!(:player_season_stat) { FactoryGirl.create(:player_season_stat, player: player, season: transfer_day.transfer_window.season) }
+      let!(:player_season_info) { FactoryGirl.create(:player_season_info, player: player, season: transfer_day.transfer_window.season) }
+      let!(:transfer) { FactoryGirl.create(:transfer, transfer_day: transfer_day, player: player) }
+      let!(:transfer_bid) { FactoryGirl.create(:transfer_bid, transfer_day: transfer_day, player: player) }
+      let!(:transfer_listing) { FactoryGirl.create(:transfer_listing, transfer_day: transfer_day, player: player) }
+      
+      before { visit show_transfer_bids_transfer_day_path(transfer_day) }
+      
+      it { is_expected.to have_selector("table.transfers") }
+      it { is_expected.to have_selector("table.transfer-bids") }
+      it { is_expected.to have_selector("table.transfer-listings") }
+    end          
+  end
+
   
 end
