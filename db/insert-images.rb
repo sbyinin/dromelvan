0acd08baecad7a62c8853731c5c28ccb46a00ff8
dromelvan <dromelvan@aland.net>
2016-08-04 22:58:19 +0300

    Player photos.

--- db/insert-images.rb
+++ db/insert-images.rb
@@ -1,30 +1,33 @@
+require 'fileutils'
 # Run this with: rails runner "eval(File.read 'db/insert-images.rb')"
 
-Dir.entries("project/graphics/player-photos").each do |file_name|
+Dir.entries("project/graphics/skysports-player-photos").each do |file_name|
   found = false
   name = file_name.sub('.jpg','').parameterize
   player = Player.where(parameterized_name: name).first
   if !player.nil? #&& player.player_photo_file_name.nil?
     found = true
-    player.player_photo = File.new("project/graphics/player-photos/#{file_name}")
+    player.player_photo = File.new("project/graphics/skysports-player-photos/#{file_name}")
     player.save
   else
     players = Player.named(name)
     if players.size == 1
       player = players.first
-      if true # player.player_photo_file_name.nil?
+      #if player.player_photo_file_name.nil?
         found = true
-        player.player_photo = File.new("project/graphics/player-photos/#{file_name}")
+        player.player_photo = File.new("project/graphics/skysports-player-photos/#{file_name}")
         player.save              
-      end
+      #end
     end
   end
   
   if !found
     puts("Did not find #{file_name}")
+  else
+    FileUtils.mv("project/graphics/skysports-player-photos/#{file_name}", "project/graphics/moved/#{file_name}")
   end
 end
-
+=begin
 Dir.entries("project/graphics/club-crests").each do |file_name|
   code = file_name.sub('.png','')
   team = Team.where(code: code).first
@@ -33,3 +36,4 @@ Dir.entries("project/graphics/club-crests").each do |file_name|
     team.save
   end  
 end
+=end
